380734f1055007e2e437da3720e9d37f2c1d68cd
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-07 10:09:49 -0300

    now its possible to sort countries by name and population

--- src/components/CountryTable/CountryTable.js
+++ src/components/CountryTable/CountryTable.js
@@ -1,24 +1,86 @@
-import styles from './CountryTable.module.css'
+import {
+  KeyboardArrowDownRounded,
+  KeyboardArrowUpRounded,
+} from "@material-ui/icons";
+import styles from "./CountryTable.module.css";
+import { useState } from "react";
+const orderBy = (countries,value, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) =>
+      a[value] > b[value] ? 1 : -1
+    );
+  }
+  if (direction == "desc") {
+    return [...countries].sort((a, b) =>
+      a[value] > b[value] ? -1 : 1
+    );
+  }
+  return countries;
+};
 
-const CountryTable = ({countries}) => {
+const SortArrow = ({ direction }) => {
+  if (!direction) {
+    return <></>;
+  }
+  if (direction == "desc") {
     return (
-        <div>
-            <div className={styles.heading}>
-                <button className={styles.heading_name}> 
-                    <div>Name</div>
-                </button>
-                <button className={styles.heading_population}> 
-                    <div>Population</div>
-                </button>
-            </div>
-            {countries.map((country) =>(
-                <div className={styles.row}> 
-                    <div className={styles.name}>{country.name}  </div>
-                    <div className={styles.population}>{country.population}</div>
-                </div>
-            ))}
-        </div>
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowDownRounded color="inherit" />
+      </div>
+    );
+  } else {
+    return(
+    <div className={styles.heading_arrow}>
+      <KeyboardArrowUpRounded color="inherit" />
+    </div>
     )
-}
+  }
+};
+const CountryTable = ({ countries }) => {
+  const [direction, setDirection] = useState();
+  const [value, setValue] = useState();
+  const orderedCountries = orderBy(countries,value,direction);
 
-export default CountryTable
+  const switchDirection = () => {
+    if (!direction) {
+      setDirection("desc");
+    } else if (direction === "desc") {
+      setDirection("asc");
+    } else {
+      setDirection(null);
+    }
+  };
+
+  const setValueandDirection = (value) => {
+    switchDirection();
+    setValue(value);
+  };
+  return (
+    <div>
+      <div className={styles.heading}>
+        <button
+          className={styles.heading_name}
+          onClick={() => setValueandDirection("name")}
+        >
+          <div>Name</div>
+          <SortArrow direction={direction} />
+        </button>
+        <button
+          className={styles.heading_population}
+          onClick={() => setValueandDirection("population")}
+        >
+          <div>Population</div>
+          <SortArrow direction={direction} />
+        </button>
+      </div>
+      {orderedCountries.map((country) => (
+        <div className={styles.row}>
+          <div className={styles.name}>{country.name} </div>
+          <div className={styles.population}>{country.population}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CountryTable;
