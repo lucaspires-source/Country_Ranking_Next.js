47786bf48fbbb5f3d8374838bf63a0ce4aeca716
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-07 17:09:30 -0300

    Remove all module files

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,7 @@
   
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import styles from "./Country.module.css";
+import styles from './Country.Module.css'
 
 const getCountry = async (id) => {
     const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
