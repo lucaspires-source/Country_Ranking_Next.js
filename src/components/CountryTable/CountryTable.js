3abae35c137d71527ee4ac2a557afc39e4f135c9
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-07 11:54:57 -0300

    created serversideprops function

--- src/components/CountryTable/CountryTable.js
+++ src/components/CountryTable/CountryTable.js
@@ -4,16 +4,13 @@ import {
 } from "@material-ui/icons";
 import styles from "./CountryTable.module.css";
 import { useState } from "react";
-const orderBy = (countries,value, direction) => {
+import Link from 'next/Link';
+const orderBy = (countries, value, direction) => {
   if (direction === "asc") {
-    return [...countries].sort((a, b) =>
-      a[value] > b[value] ? 1 : -1
-    );
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
   }
   if (direction == "desc") {
-    return [...countries].sort((a, b) =>
-      a[value] > b[value] ? -1 : 1
-    );
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
   }
   return countries;
 };
@@ -29,17 +26,17 @@ const SortArrow = ({ direction }) => {
       </div>
     );
   } else {
-    return(
-    <div className={styles.heading_arrow}>
-      <KeyboardArrowUpRounded color="inherit" />
-    </div>
-    )
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowUpRounded color="inherit" />
+      </div>
+    );
   }
 };
 const CountryTable = ({ countries }) => {
   const [direction, setDirection] = useState();
   const [value, setValue] = useState();
-  const orderedCountries = orderBy(countries,value,direction);
+  const orderedCountries = orderBy(countries, value, direction);
 
   const switchDirection = () => {
     if (!direction) {
@@ -74,10 +71,12 @@ const CountryTable = ({ countries }) => {
         </button>
       </div>
       {orderedCountries.map((country) => (
-        <div className={styles.row}>
-          <div className={styles.name}>{country.name} </div>
-          <div className={styles.population}>{country.population}</div>
-        </div>
+        <Link href={`/country/${country.alpha3Code}`}>
+          <div className={styles.row}>
+            <div className={styles.name}>{country.name} </div>
+            <div className={styles.population}>{country.population}</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
