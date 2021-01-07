b4cb1d05226459e2075ad1104a72e5030e8764e6
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-07 10:31:30 -0300

    now its possible to sort countries by name and population

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,42 @@
+import { useState } from "react";
 import CountryTable from "../components/CountryTable/CountryTable";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
-import styles from '../styles/Home.module.css'
-export default function Home({countries}) {
+import styles from "../styles/Home.module.css";
+export default function Home({ countries }) {
+  const [keyword, setKeyWord] = useState("");
 
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setKeyWord(e.target.value.toLowerCase());
+  };
   return (
     <>
       <Layout>
         <div className={styles.counts}>Found {countries.length} Countries </div>
-      <SearchInput placeholder="Filter By Name, Region or Subregion"/>
-      <CountryTable countries={countries}/>  
+        <SearchInput
+          placeholder="Filter By Name, Region or Subregion"
+          onChange={onInputChange}
+        />
+        <CountryTable countries={filteredCountries} />
       </Layout>
-
-
-    </>  
-  )
+    </>
+  );
 }
 
-export const getStaticProps = async () =>{
+export const getStaticProps = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
-  return{
-    props:{
-        countries,
+  return {
+    props: {
+      countries,
     },
   };
 };
